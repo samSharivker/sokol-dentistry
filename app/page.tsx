02cb011b894832affb5264cd61f0ed1df1f54790
Samuel Sharivker <sjsharivker@gmail.com>
2025-12-23 10:11:14 -0500

    responsive header

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-full">
       <p>Home Page</p>
     </div>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,24 @@
 export default function Header() {
   return (
-    <div className="bg-blue-400 text-shadow-2xs text-white flex justify-between items-center min-h-20 px-20">
-      <p className="text-3xl">Sokol Dentistry</p>
-      <div className="flex-col justify-center items-start text-md">
-        <p>(718)745-6699</p>
-        <p>Brooklyn, NY</p>
+    <header className="bg-blue-400 text-white text-shadow-2xs">
+      <div
+        className="
+          max-w-7xl mx-auto
+          flex flex-col items-center text-center
+          sm:flex-row sm:justify-between sm:items-center sm:text-left
+          px-4 sm:px-8 lg:px-20
+          py-2 sm:py-4
+        "
+      >
+        <p className="text-lg sm:text-3xl lg:text-4xl font-semibold leading-tight">
+          Sokol Dentistry
+        </p>
+
+        <div className="mt-0.5 sm:mt-0 text-[11px] sm:text-base leading-tight">
+          <p>(718) 745-6699</p>
+          <p>Brooklyn, NY</p>
+        </div>
       </div>
-    </div>
+    </header>
   );
 }
