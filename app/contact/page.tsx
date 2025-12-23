39845fd1821c52f4278ca8357b294390d162346e
Samuel Sharivker <sjsharivker@gmail.com>
2025-12-23 11:30:14 -0500

    added leaflet integration

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,18 @@
+"use client"
+
+import dynamic from "next/dynamic";
+
+const MapClient = dynamic(() => import("../../components/MapComponent"), {
+  ssr: false,
+});
+
 export default function Contact() {
   return (
-    <div>
+    <div className="flex flex-col items-center gap-4 p-6">
       <p>Contact Page</p>
+      <div className="w-full max-w-3xl h-96">
+        <MapClient />
+      </div>
     </div>
   );
 }
---
+++ components/MapComponent.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
+const position: [number, number] = [40.628172, -74.019163];
+
+export default function MapComponent() {
+  return (
+    <MapContainer center={position} zoom={18} style={{ height: "100%", width: "100%" }}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={position}>
+        <Popup>
+          636 74th Street Brooklyn, NY, 11209
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
