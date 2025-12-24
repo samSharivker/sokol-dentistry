2e101b9697e14ff8643f8e90f3aab4af129b77f8
Samuel Sharivker <sjsharivker@gmail.com>
2025-12-24 12:49:47 -0500

    bridges

--- app/treatments/bridges/page.tsx
+++ app/treatments/bridges/page.tsx
@@ -1,9 +1,109 @@
-"use client"
+"use client";
 
 export default function Bridges() {
   return (
-    <div>
-      <p>Bridges Page</p>
+    <div className="min-h-screen w-full flex flex-col items-center overflow-x-hidden">
+      <div className="w-full flex flex-col gap-5 px-6 py-10 md:px-20 md:py-20">
+        <p className="font-bold text-3xl sm:text-4xl md:text-5xl">Dental Bridges</p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">
+          All of your teeth play an important role in speaking, chewing and in maintaining proper
+          alignment of other teeth. Tooth loss doesn’t necessarily have to occur as you age, but if
+          you do lose teeth they must be replaced to maintain proper function of your mouth.
+          Fortunately, there are options for correcting tooth loss.
+        </p>
+      </div>
+
+      <div className="w-full flex flex-col gap-5 bg-slate-200 px-6 py-10 md:px-20 md:py-20">
+        <p className="font-bold text-3xl sm:text-4xl md:text-5xl">Dental Bridge Options</p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">
+          A bridge a device used to replace missing teeth attaches artificial teeth to adjacent
+          natural teeth, called abutment teeth. Bridges are either permanently attached (fixed
+          bridges), or they can be removable.
+        </p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">
+          Fixed bridges are applied by either placing crowns on the abutment teeth or by bonding the
+          artificial teeth directly to the abutment teeth. Removable bridges are attached to the
+          teeth with metal clasps or by precision attachments.
+        </p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">
+          If you’re missing one or more teeth, you may be aware of their importance to your
+          appearance and dental health. Your teeth work together for many daily functions from
+          eating to speaking. With missing teeth, its difficult to do these things. Missing teeth
+          can and should be replaced. Fixed bridges are a great way to restore your dental health
+          and appearance.
+        </p>
+      </div>
+
+      <div className="w-full flex flex-col gap-5 px-6 py-10 md:px-20 md:py-20">
+        <p className="font-bold text-3xl sm:text-4xl md:text-5xl">
+          What exactly is a bridge or fixed partial denture?
+        </p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">
+          A bridge (fixed partial denture) is a device which fills the gap where teeth are absent.
+          Fixed bridges are bonded into place and can only be removed by a dental professional.
+          Removable bridges, as the name implies, can be taken out and cleaned. Fixed bridges offer
+          more stability than their removable counterparts.
+        </p>
+      </div>
+
+      <div className="w-full flex flex-col gap-5 bg-slate-200 px-6 py-10 md:px-20 md:py-20">
+        <p className="font-bold text-3xl sm:text-4xl md:text-5xl">Why do I need a bridge?</p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">
+          Oral functionality and appearance are important reasons for wearing a bridge. A bridge
+          helps support your lips and cheeks. The loss of a back tooth may cause your mouth to sink
+          and your face to look older.
+        </p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">
+          Dental health is the most important reason for a bridge. Teeth were designed to complement
+          each other. Unusual stresses are placed on the gums and other oral tissues when teeth are
+          missing, causing a number of potentially harmful disorders.
+        </p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">
+          Increased risk of gum disease has proven to be one of the worst side effects of missing
+          teeth and can be minimized with a bridge.
+        </p>
+        <p className="font-bold text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">
+          Missing teeth can cause speech disorders as they are used to make many of the sounds we
+          use to speak clearly.
+        </p>
+      </div>
+
+      <div className="w-full flex flex-col gap-5 px-6 py-10 md:px-20 md:py-20">
+        <p className="font-bold text-3xl sm:text-4xl md:text-5xl">How is a bridge attached?</p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">
+          The attachment procedure usually takes two or three appointments to complete. At the first
+          appointment Dr. Sokol will prepare the teeth on either side of the gap by removing a
+          portion of the enamel and dentin.
+        </p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">
+          Since the bridge must be fabricated very precisely to ensure correct bite and to match the
+          opposing tooth, impressions of the teeth are taken and sent to a lab where the bridge will
+          be constructed.
+        </p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">
+          Fixed bridges are typically cemented to the natural teeth next to the space left by the
+          missing tooth. A pontic (false tooth) replaces the lost tooth. Crowns, which are cemented
+          onto the natural teeth, provide support for the bridge.
+        </p>
+      </div>
+
+      <div className="w-full flex flex-col gap-5 bg-slate-200 px-6 py-10 md:px-20 md:py-20">
+        <p className="font-bold text-3xl sm:text-4xl md:text-5xl">What materials are used?</p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">
+          Bridges can be constructed from gold alloys, non-precious alloys, porcelain, or a
+          combination of these materials. Porcelain is often bonded to either precious or
+          non-precious metal.
+        </p>
+      </div>
+
+      <div className="w-full flex flex-col gap-5 px-6 py-10 md:px-20 md:py-20 mb-10">
+        <p className="font-bold text-3xl sm:text-4xl md:text-5xl">How do I take care of my bridge?</p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">
+          A strict regimen of brushing and flossing will keep the bridge and surrounding teeth
+          clean. This is of critical importance as the bridge relies on the neighboring teeth for
+          support.
+        </p>
+      </div>
     </div>
   );
 }
