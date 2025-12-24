9fa52c20ad3804e1d05c47cdf2df7d12e19ace33
Samuel Sharivker <sjsharivker@gmail.com>
2025-12-24 14:00:59 -0500

    implants

--- app/treatments/implants/page.tsx
+++ app/treatments/implants/page.tsx
@@ -1,9 +1,21 @@
-"use client"
+"use client";
 
 export default function Implants() {
   return (
-    <div>
-      <p>Implants Page</p>
+    <div className="min-h-screen w-full flex flex-col items-center overflow-x-hidden">
+      <div className="w-full flex flex-col gap-5 px-6 py-10 md:px-20 md:py-20">
+        <p className="font-bold text-3xl sm:text-4xl md:text-5xl">Dental Implants</p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">
+          Dental implants are designed to provide a foundation for replacement teeth that look, feel and function like natural teeth. The person who has lost teeth regains the ability to eat virtually anything and can smile with confidence, knowing that teeth appear natural and that facial contours will be preserved. The implants are tiny titanium posts which are placed into the jawbone where teeth are missing. The bone bonds with the titanium, creating a strong foundation for artificial teeth. In addition, dental implants can help preserve facial structure, preventing the bone deterioration which occurs when teeth are missing.
+        </p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">Dental implants are changing the way people live! With them, people are rediscovering the comfort and confidence to eat, speak, laugh and enjoy life.</p>
+      </div>
+      <div className="w-full flex flex-col gap-5 px-6 py-10 md:px-20 md:py-20 bg-slate-200">
+        <p className="font-bold text-3xl sm:text-4xl md:text-5xl">Evaluation for Dental Implants</p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">Dental implants are metal anchors, which act as tooth root substitutes. They are surgically placed into the jawbone. Small posts are then attached to the implant, which protrude through the gums. These posts provide stable anchors for artificial replacement teeth.</p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">For most patients, the placement of dental implants involves two surgical procedures. First, implants are placed within your jawbone. For the first three to six months following surgery, the implants are beneath the surface of the gums, gradually bonding with the jawbone. You should be able to wear temporary dentures and eat a soft diet during this time. At the same time, your restorative dentist designs the final bridgework or denture, which will ultimately improve both function and aesthetics.</p>
+        <p className="text-base sm:text-lg md:text-xl leading-7 sm:leading-8 max-w-5xl">After the implant has bonded to the jawbone, the second phase begins. The surgeon will uncover the implants and attach a small healing collar. Dr. Sokol can then start making your new teeth. An impression must be taken. Then posts or attachments can be connected to the implants. The replacement teeth are then made over the posts or attachments. The entire procedure usually takes six to eight months. Most patients do not experience any disruption in their daily life.</p>
+      </div>
     </div>
   );
 }
